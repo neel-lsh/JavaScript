9928ac4e20e95474b34e790dba6f5291390dbc55
neel-lsh <147874633+neel-lsh@users.noreply.github.com>
2025-01-29 18:49:03 +0000

    comparision and operator

---
+++ Basic/comparision.js
@@ -0,0 +1,7 @@
+console.log("2">3);
+console.log(null>=0);
+console.log(null <0);
+console.log(null==0);
+console.log("2"==2);
+console.log("2"===2);
+//===  Strict equality (checks value & type)
---
+++ Basic/operation.js
@@ -0,0 +1,12 @@
+let value=3;
+let neg=-value;
+console.log(neg);
+console.log(2+2);
+console.log(2-2);
+console.log(2*2);
+console.log(23%4);
+let  str1="Hello";
+let str2=" Nilesh";
+let str3=str1+str2;
+console.log(str3);
+

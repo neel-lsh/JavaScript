e4f425cc83da6877ef3c0e1b0937f01848d96054
neel-lsh <147874633+neel-lsh@users.noreply.github.com>
2025-02-03 18:42:15 +0000

    String

---
+++ Basic/DataType1.js
@@ -0,0 +1,22 @@
+// primitive 
+// 7 type : String ,Number ,Boolean ,null,undefine,Symbol,BigInt
+
+//it's  dynamically type language
+
+// Reference type/ Non-primitive
+// Array,Object,Function
+const hero=["h1","h2"];
+console.log(hero);
+
+let obj1={
+    name:"Nilesh",
+    age:21,
+}
+console.log(obj1);
+let fun=function(){
+    console.log("Hello world");
+}
+fun()
+//++++++++++++++++++++++++++++++++++++++++++++++++++
+// Stack (primitive),Heap (Non-Premitive)
+ 
---
+++ Basic/String.js
@@ -0,0 +1,11 @@
+const Name = "Nilesh";
+const repcount = 10;
+
+console.log(Name + repcount); // Correct variable usage
+console.log(`Hello My name is ${Name} and my Repo count is ${repcount}`);
+ // Use backticks
+ // another way ti implement String 
+ const gameName=new String('COD');
+ console.log(gameName);
+ console.log(gameName[0]);
+ console.log(gameName.indexOf('O'));

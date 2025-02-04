a26108b09b3d796629f8277c152046635a456c76
neel-lsh <147874633+neel-lsh@users.noreply.github.com>
2025-02-04 17:59:06 +0000

    String

--- Basic/String.js
+++ Basic/String.js
@@ -1,11 +1,22 @@
 const Name = "Nilesh";
 const repcount = 10;
 
-console.log(Name + repcount); // Correct variable usage
-console.log(`Hello My name is ${Name} and my Repo count is ${repcount}`);
- // Use backticks
- // another way ti implement String 
- const gameName=new String('COD');
- console.log(gameName);
- console.log(gameName[0]);
- console.log(gameName.indexOf('O'));
+console.log(Name + repcount); 
+console.log(`Hello, my name is ${Name} and my Repo count is ${repcount}`);
+
+const gameName = new String('Hello World       ');
+console.log(gameName);
+console.log(gameName[0]);
+console.log(gameName.indexOf('o')); // Fixed case sensitivity
+console.log(gameName.length);
+console.log(gameName.toLowerCase());
+console.log(gameName.charAt(2));
+
+const newString = gameName.substring(0, 5);
+console.log(newString);
+
+const anotherString = gameName.slice(4, 6); // Fixed incorrect indices
+console.log(anotherString);
+
+const trim1 = gameName.trim();
+console.log(trim1);

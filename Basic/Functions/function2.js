d0e135aa1d7383dd9dab63a7c108a749fef8add6
neel-lsh <147874633+neel-lsh@users.noreply.github.com>
2025-07-15 07:09:09 +0000

    commit

---
+++ Basic/Functions/function2.js
@@ -0,0 +1,22 @@
+function calculateprice(val1,val2,...num1){
+    return num1
+
+
+}
+console.log(calculateprice(10, 20, 30, 40, 50));
+
+const user={
+    username: "Nilesh",
+    price:15
+}
+function handleobject(obj){
+    console.log(`username is ${obj.username} and price is ${obj.price}`);
+}
+handleobject(user);
+handleobject({username: "John", price: 25});
+
+const arr1=[1, 2, 3, 4, 5];
+function secondval(arr){
+  return arr[1];
+}
+console.log(secondval(arr1));

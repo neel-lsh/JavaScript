9c71c5dc0e6da5852b17b5150969da1352e0ea60
neel-lsh <147874633+neel-lsh@users.noreply.github.com>
2025-07-22 12:22:17 +0000

    commit

---
+++ Loops/Loop4.js
@@ -0,0 +1,11 @@
+ const arr=[1,2,3,4,5,6,7,9]
+
+ const newarr=arr.map((num) => num * 2)
+                .map((num) => num + 1)
+                .filter((num) => num > 10)
+console.log(newarr); 
+// Logs: [11, 13, 15, 19
+
+const mynums = [1, 2, 3, 4];
+const total= mynums.reduce((accumulator, currentValue) => accumulator + currentValue, 0);
+console.log(total); // Logs: 10
